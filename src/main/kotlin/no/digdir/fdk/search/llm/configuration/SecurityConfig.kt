7d34e707a7b79a44246e33d62273617bf06b5bc7
Jeff Reiffers <jeffreiffers@gmail.com>
2024-08-22 10:26:09 +0200

    chore: use JSON arguments for CMD in Dockerfile and add debug logging for cors

--- src/main/kotlin/no/digdir/fdk/search/llm/configuration/SecurityConfig.kt
+++ src/main/kotlin/no/digdir/fdk/search/llm/configuration/SecurityConfig.kt
@@ -1,5 +1,7 @@
 package no.digdir.fdk.search.llm.configuration
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -24,6 +26,9 @@ open class SecurityConfig(
                     config.maxAge = 3600L
                     config.allowedOriginPatterns = corsOriginPatterns.toList()
                     config.allowedMethods = listOf("GET", "POST", "OPTIONS")
+
+                    logger.debug("CORS configuration allowed origin patterns: {}", config.allowedOriginPatterns)
+
                     config
                 }
             }
@@ -34,5 +39,9 @@ open class SecurityConfig(
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
         return http.build()
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(SecurityConfig::class.java)
     }
 }
