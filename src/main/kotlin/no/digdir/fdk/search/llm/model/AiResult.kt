38b56f40a9717852d055d5e7b94428a82cd4f423
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-08-06 13:11:03 +0200

    refactor: move AIResultHit and AIResult into AiResult.kt

---
+++ src/main/kotlin/no/digdir/fdk/search/llm/model/AiResult.kt
@@ -0,0 +1,16 @@
+package no.digdir.fdk.search.llm.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class AIResultHit(
+    val id: String,
+    val name: String,
+    val reason: String
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class AIResult(
+    val sensitive: Boolean,
+    val hits: List<AIResultHit>
+)
