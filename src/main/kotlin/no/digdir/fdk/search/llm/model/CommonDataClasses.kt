fc77bed2cd60839c06a2035b78a62e060b00695b
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-08-06 13:11:03 +0200

    refactor: move AIResultHit and AIResult into AiResult.kt

--- src/main/kotlin/no/digdir/fdk/search/llm/model/CommonDataClasses.kt
+++ src/main/kotlin/no/digdir/fdk/search/llm/model/CommonDataClasses.kt
@@ -151,16 +151,3 @@ data class Temporal(
     val startDate: String?,
     val endDate: String?,
 )
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class AIResultHit(
-    val id: String,
-    val name: String,
-    val reason: String
-)
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class AIResult(
-    val sensitive: Boolean,
-    val hits: List<AIResultHit>
-)
